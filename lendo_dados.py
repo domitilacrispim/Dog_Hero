37b568bc169b870851085ed955fb44ccf38b15c4
domitilacrispim <domitilacrispim@gmail.com>
2019-10-31 15:46:08 -0300

    Update lendo_dados.py

--- lendo_dados.py
+++ lendo_dados.py
@@ -1,3 +1,4 @@
+
 import csv
 with open('dados.csv') as csv_file:
     csv_reader = csv.reader(csv_file)
@@ -8,20 +9,23 @@ with open('dados.csv') as csv_file:
     aux=[]
     flag=0
     probabilidade = []
-    for coluna in range(0,1):
+    for coluna in range(0,33):
         tamanho.append(0)
         for row in csv_reader:
+            flag=0
             if row[4]=="true":
-                for elemento in legenda:
-                    if row[coluna]==legenda[elemento]:
-                        probabilidade[elemento]=probabilidade[elemento]+1
+                for elemento in aux:
+                    if row[coluna]==elemento:
+                        probabilidade[aux.index(elemento)]=probabilidade[aux.index(elemento)]+1
                         flag=1
                 if flag==0:
                     aux.append(row[coluna])
                     probabilidade.append(1)
                     tamanho[coluna]=tamanho[coluna]+1
         legenda.append(aux)
+        aux=[]
         mapeamento.append(probabilidade)
+        probabilidade=[]
         flag=0
 print (legenda)
-
+print (probabilidade)
