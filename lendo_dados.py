4c9ab1c54803ec149b1a39998c31e9da201f5506
domitilacrispim <domitilacrispim@gmail.com>
2019-11-01 19:02:57 -0300

    Update lendo_dados.py

--- lendo_dados.py
+++ lendo_dados.py
@@ -1,31 +1,33 @@
-
 import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm, datasets
 with open('dados.csv') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    cabecalho = True
-    mapeamento=[]
-    tamanho=[]
-    legenda=[]
-    aux=[]
-    flag=0
-    probabilidade = []
-    for coluna in range(0,33):
-        tamanho.append(0)
-        for row in csv_reader:
-            flag=0
-            if row[4]=="true":
-                for elemento in aux:
-                    if row[coluna]==elemento:
-                        probabilidade[aux.index(elemento)]=probabilidade[aux.index(elemento)]+1
-                        flag=1
-                if flag==0:
-                    aux.append(row[coluna])
-                    probabilidade.append(1)
-                    tamanho[coluna]=tamanho[coluna]+1
-        legenda.append(aux)
-        aux=[]
-        mapeamento.append(probabilidade)
-        probabilidade=[]
-        flag=0
-print (legenda)
-print (probabilidade)
+ csv_reader = csv.reader(csv_file)
+ dados=[]
+ total=0
+ for row in csv_reader:
+  total=total+1
+  dados.append(row)
+ mapeamento=[]
+ sucesso=[]
+ fracasso=[]
+ for col in range (0,31):
+  mapeamento.append([])
+  fracasso.append([])
+  sucesso.append([])
+  for row in dados:
+   if row[32]=="true":
+    if row[col] in mapeamento[col]:
+     sucesso[col][mapeamento[col].index(row[col])]=sucesso[col][mapeamento[col].index(row[col])]+1
+    else:
+     mapeamento[col].append(row[col])
+     sucesso[col].append(1)
+     fracasso[col].append(0)
+   if row[32]=="false":
+    if row[col] in mapeamento[col]:
+     fracasso[col][mapeamento[col].index(row[col])]=fracasso[col][mapeamento[col].index(row[col])]+1
+    else:
+     mapeamento[col].append(row[col])
+     fracasso[col].append(1)
+     sucesso[col].append(0)
